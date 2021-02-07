d1ad7adec06e6ed7a11df4860b342e2edfef0e04
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-02-07 21:16:30 +0530

    Added hbs files

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,10 +1,32 @@
 const path = require("path");
 const express = require("express");
-
 const app = express();
 
-const dirPath = path.join(__dirname, "../public");
-app.use(express.static(dirPath));
+// const dirPath = path.join(__dirname, "../public");
+// app.use(express.static(dirPath));
+
+app.set("view engine", "hbs");
+
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Home Page",
+		author: "Brad",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Page",
+		author: "Brad",
+	});
+});
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help Page",
+		mssg: "Contact 456 for help related queries",
+	});
+});
 
 app.get("/weather", (req, res) => {
 	res.send({
