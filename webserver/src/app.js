53cce29d80c517bf90b4d289b7d497b67d93f9c5
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-02-08 22:28:27 +0530

    Added hbs templates

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,30 +1,39 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 
-// const dirPath = path.join(__dirname, "../public");
-// app.use(express.static(dirPath));
-
+//path variables
+const dirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+//static content (css, js, images)
+app.use(express.static(dirPath));
+//dynamic content (hbs files)
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
 
 app.get("", (req, res) => {
 	res.render("index", {
-		title: "Home Page",
-		author: "Brad",
+		title: "Weather",
+		author: "Andrew",
 	});
 });
 
 app.get("/about", (req, res) => {
 	res.render("about", {
-		title: "About Page",
-		author: "Brad",
+		title: "About",
+		author: "Andrew",
 	});
 });
 
 app.get("/help", (req, res) => {
 	res.render("help", {
-		title: "Help Page",
-		mssg: "Contact 456 for help related queries",
+		title: "Help",
+		author: "Andrew",
+		mssg: "Contact 999 for help related queries",
 	});
 });
 
@@ -32,6 +41,22 @@ app.get("/weather", (req, res) => {
 	res.send({
 		forecast: 50,
 		location: "Paris",
+	});
+});
+
+app.get("/help/*", (req, res) => {
+	res.render("errorPage", {
+		title: "Error",
+		author: "Andrew",
+		mssg: "404 error on /help - Page not found",
+	});
+});
+
+app.get("*", (req, res) => {
+	res.render("errorPage", {
+		title: "Error",
+		author: "Andrew",
+		mssg: "404 error - Page not found",
 	});
 });
 
