ea4fee1e371f4bfb010703fff8cf0556d2b9e020
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-04-14 21:29:39 +0530

    Added connection and disconnection for client

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,17 +1,38 @@
 const socket = io();
 const chatForm = document.querySelector(".chat-form");
 const message = document.querySelector("#message");
-
-chatForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	socket.emit("sendMessage", message.value);
-	message.value = "";
-});
+const locationBtn = document.querySelector(".location");
 
 socket.on("message", (mssg) => {
 	console.log(mssg);
 });
 
-socket.on("receiveMessage", (mssg) => {
-	console.log(mssg);
+chatForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	chatForm.setAttribute("disabled", "disabled");
+	socket.emit("sendMessage", message.value, (error) => {
+		chatForm.removeAttribute("disabled");
+		message.value = "";
+		message.focus();
+		if (error) {
+			return console.log(error);
+		}
+		console.log("Message Delievered :)");
+	});
+});
+
+locationBtn.addEventListener("click", () => {
+	if (!navigator.geolocation) {
+		return alert("Geolocation not supported on browser.");
+	}
+
+	locationBtn.setAttribute("disabled", "disabled");
+	navigator.geolocation.getCurrentPosition((position) => {
+		const { latitude, longitude } = position.coords;
+		socket.emit("sendLocation", { latitude, longitude }, () => {
+			locationBtn.removeAttribute("disabled");
+			console.log("Location Shared :)");
+		});
+	});
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,11 +13,37 @@ const dirPath = path.join(__dirname, "../public/");
 
 app.use(express.static(dirPath));
 
+//connect with new client
 io.on("connection", (socket) => {
+	//emit to particular connection
 	socket.emit("message", "Welcome to Chat App.");
+	//emit to all except particular connection
+	socket.broadcast.emit("message", "New User has joined the chat.");
 
-	socket.on("sendMessage", (message) => {
-		io.emit("receiveMessage", message);
+	socket.on("sendMessage", (message, callback) => {
+		//check badwords
+		const filter = new Filter();
+		if (filter.isProfane(message)) {
+			return callback("Profanity is not allowed.");
+		}
+
+		//emit to all connections
+		io.emit("message", message);
+		callback();
+	});
+
+	//when client disconnects
+	socket.on("disconnect", () => {
+		io.emit("message", "A user has left the chat.");
+	});
+
+	//client location
+	socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+		socket.broadcast.emit(
+			"message",
+			`🗺️ https://www.google.com/maps?q=${latitude},${longitude}`
+		);
+		callback();
 	});
 });
 
